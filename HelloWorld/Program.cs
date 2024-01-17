ad6e7cba5e8a843ae63e578bf6399246938bea14
@21izah <izahmichael@outlook.com>
2024-01-17 16:17:56 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] my_int_list = {1,2,3,4,5};
+Console.WriteLine(my_int_list[2]);
+const double pi = 3.142;
+int number;
+int my_number = 2;
+string my_name = "Michael";
+bool my_bool = false;
+
+class Animal{
+
+    public int Value { get; set; }
+
+    public int add_number(){
+        return Value;
+    }
+}
